b1759f2b1d197c2cd3ccf7ebfc044297895d2b51
Bravin Murambi <bravvjr@gmail.com>
2025-05-14 15:41:54 +0300

    fix

--- src/pages/ViewAllPage/ViewAllPage.jsx
+++ src/pages/ViewAllPage/ViewAllPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import Grid from "../../components/Grid";
+import Grid from "../../components/grid";
 import "./styles/ViewAllPage.css";
 // import NetflixWallpaper from "../../assets/netflix.jpg";
 
